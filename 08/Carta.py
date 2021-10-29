c72dbe7ca91ff831d1d4ace00c40b603ac8287a0
Rafa <rafaelatillmann.rt@gmail.com>
2021-10-29 11:33:11 -0300

    exercise 08

---
+++ 08/Carta.py
@@ -0,0 +1,22 @@
+from AbstractCarta import *
+from Personagem import *
+
+
+class Carta(AbstractCarta):
+
+    def __init__(self, personagem: Personagem):
+        self.__personagem = personagem
+
+    '''
+    Soma e retorna todos os valores dos atributos do personagem da Carta
+    @return Retorna o somatorio de todos os atributos do personagem da Carta
+    '''
+
+    def valor_total_carta(self):
+        valor = self.__personagem.energia + self.__personagem.habilidade + \
+            self.__personagem.resistencia + self.__personagem.velocidade
+        return valor
+
+    @property
+    def personagem(self) -> Personagem:
+        return self.__personagem
